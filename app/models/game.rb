8bc0fcbf434536651fa58456a24c4700b909b58c
Luis Del Giudice <delgiudices@Luiss-MacBook-Pro.local>
2014-08-12 15:55:59 -0400

    Added game tests

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,14 @@
 class Game < ActiveRecord::Base
+
+	validates :scheduled_start_time, presence: true
+
+	def save
+		if super
+			if self.end_time < self.start_time
+				return false
+			end
+		end
+	end
+
+
 end
