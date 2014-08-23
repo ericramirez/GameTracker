8e043f82e6560180903839b0ec16d8898be57384
Hector Montero <cerberos.025@gmail.com>
2014-08-23 20:34:59 +0000

    Cleaning the app

--- app/models/team.rb
+++
@@ -1,3 +0,0 @@
-class Team < ActiveRecord::Base
-	validates :name, presence: true
-end
--- db/migrate/20140813150511_add_team_ref_to_game.rb
+++
@@ -1,5 +0,0 @@
-class AddTeamRefToGame < ActiveRecord::Migration
-  def change
-    add_reference :games, :team1, index:true
-  end
-end
