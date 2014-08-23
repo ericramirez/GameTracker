bed5d246db660fdfcdcf057d75b0a96acef477ad
Luis Del Giudice <luis.dg19@gmail.com>
2014-08-23 19:17:35 -0400

    Fixed some indentationw

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,14 +1,14 @@
 class Game < ActiveRecord::Base
-    belongs_to :away_team, class_name: 'Team', inverse_of: :away_games
+  belongs_to :away_team, class_name: 'Team', inverse_of: :away_games
 	belongs_to :home_team, class_name: 'Team', inverse_of: :home_games
 
 	validates :away_team, :home_team, presence: true, inclusion: { in: Team.all }
 	validate  :we_can_play?
 	validates :scheduled_start_time, presence: true
-    
-    def we_can_play?
-     	if away_team_id == home_team_id
-       	    errors.add(:home_team_id, "must be different from the away team")
-        end
+
+  def we_can_play?
+    if away_team_id == home_team_id
+      errors.add(:home_team_id, "must be different from the away team")
     end
+  end
 end
