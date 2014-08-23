2a9960f4e61b3b686a13cd9d6efbd5390689376e
Hector Montero <cerberos.025@gmail.com>
2014-08-23 21:31:55 +0000

    Player added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823211928) do
+ActiveRecord::Schema.define(version: 20140823213029) do
 
   create_table "games", force: true do |t|
     t.integer  "away_team_id"
